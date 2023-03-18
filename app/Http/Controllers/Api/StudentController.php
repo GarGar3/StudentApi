22f6dfd5869f2d25c75dc2ac67a8b745b8248e8a
Egmedio E. Garcia III <egmediogarcia@yahoo.com>
2023-03-18 18:06:36 +0800

    laravel Simple CRUD api

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $students = Student::all();
+        if ($students->count() > 0 ){
+            return response()->json($data = [
+                'status' => 200,
+                'students' => $students
+            ],200);
+        } else{
+            return response()->json($data = [
+                'status' => 404,
+                'message_status' => 'no record'
+            ],404);
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|string|max:191',
+            'phone' => 'required|digits:10'
+        ]);
+        if ($validator->fails()){
+            return response()->json([
+                'status' => 404,
+                'message_status' => $validator->messages()
+            ],422);
+        }else{
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+            if($student){
+                return response()->json([
+                    'status' => 200,
+                    'message_status' => 'created Succesfully'
+                ],200);
+            }else{
+                return response()->json([
+                    'status' => 500,
+                    'message_status' => 'Something went wrong'
+                ],500);
+            }
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $student = Student::find($id);
+        if($student){
+            return response()->json([
+                'status' => 200,
+                'student' => $student
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message_status' => 'No Student found'
+            ],404);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $student = Student::find($id);
+        if($student){
+            return response()->json([
+                'status' => 200,
+                'student' => $student
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message_status' => 'No Student found'
+            ],404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|string|max:191',
+            'phone' => 'required|digits:10'
+        ]);
+        if ($validator->fails()){
+            return response()->json([
+                'status' => 404,
+                'message_status' => $validator->messages()
+            ],422);
+        }else{
+            $student = Student::find($id);
+            
+            if($student){
+
+                $student->update([
+                    'name' => $request->name,
+                    'course' => $request->course,
+                    'email' => $request->email,
+                    'phone' => $request->phone,
+                ]);
+
+                return response()->json([
+                    'status' => 200,
+                    'message_status' => 'Updated Succesfully'
+                ],200);
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message_status' => 'No Student found'
+                ],404);
+            }
+        }
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $student = Student::find($id);
+            
+        if($student){
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message_status' => 'Deleted Succesfully'
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message_status' => 'No Student found'
+            ],404);
+        }
+    
+
+    }
+}
